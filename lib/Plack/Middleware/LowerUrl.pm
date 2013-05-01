173bbf23edb7853936e7540e7bf38ecf51aed73e
Logan Bell <loganbell@gmail.com>
2013-04-30 22:31:43 -0700

    Dropping versioning

--- lib/Plack/Middleware/LowerUrl.pm
+++ lib/Plack/Middleware/LowerUrl.pm
@@ -3,8 +3,6 @@ use warnings;
 package Plack::Middleware::LowerUrl;
 use parent qw( Plack::Middleware );
 
-our $VERSION = '0.001';
-
 sub call {
   my ($self, $env) = @_;
   if ( $env->{REQUEST_URI} ) {
